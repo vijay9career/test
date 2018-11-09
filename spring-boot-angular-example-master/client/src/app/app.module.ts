84694c7b1b2b2afc02e12c859a50131815a0455a
vijay9career <44897239+vijay9career@users.noreply.github.com>
2018-11-09 17:39:48 +0530

    springboot-angular project

---
+++ spring-boot-angular-example-master/client/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BeerListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
